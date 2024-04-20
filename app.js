be495c51362e0ecfd68d15e860e35452aea03194
sgeede <164593750+sgeede@users.noreply.github.com>
2024-04-20 10:54:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ function consulta(consulta){
   });
 };
 
-consulta("delete SGE_TEST.dbo.REPRESENTANTES_AUTORIZADOS where ID_REP_AUT >0")
+consulta("delete SGE_TEST.dbo.SOLICITUDES where ID_SOL >0")
 
 
 function validarFormularios(event){
